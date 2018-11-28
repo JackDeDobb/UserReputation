1f161a4c5b637470ab5ec685046352deb1715e99
Jackson DeDobbelaere <jackdedobb@me.com>
2018-11-27 19:47:42 -0600

    refactored loadData.py

--- loadData.py
+++ loadData.py
@@ -4,57 +4,53 @@ from sentimentCalculator import textBlockToSentiment
 from readabilityCalculator import textBlockToReadability
 import pandas as pd
 
-labelsTrainFile = "Task2_user_reputation_prediction/Reputation_prediction_labels_train.txt"
-dataTrainFile = "Task2_user_reputation_prediction/Reputation_prediction_data_train.txt"
-cachingFile = 'cachedMatrix.csv'
+
 contentTypes = {'Q': 1, 'A': 2, 'C': 3, 'E': 4, 'F': 5}
 
 
-def getDictionaries():
+
+def getPostsMatrix(dataTrainFile, cachingFile):
+    postsMatrix = []
+    try:
+        with open(dataTrainFile, "r") as dataTrain:
+            line = dataTrain.readline()
+            while (line):
+                textualDescription, userId, contentType, timeStamp = line.split('\t')
+                polarity, subjectivity = textBlockToSentiment(textualDescription)
+                readability, avg_length = textBlockToReadability(textualDescription)
+                postsMatrix.append([polarity, subjectivity, readability, avg_length, contentTypes[contentType], userToReps[userId] ])
+                line = dataTrain.readline()
+
+        df = pd.DataFrame(postsMatrix)
+        print()
+        if cachingFile:
+            df.to_csv(cachingFile, header=True, index=False)
+
+    except:
+        pass
+
+    return postsMatrix
+
+
+def getTrainDictionaries(labelsTrainFile, dataTrainFile, cachingFile):
     userToReps = {}
     with open(labelsTrainFile, "r") as labelsTrain:
-        line = labelsTrain.readline()
-        while (line):
-            userId, reputation = line.split('\t')
-            userToReps[userId] = int(reputation)
             line = labelsTrain.readline()
-
-
+            while (line):
+                userId, reputation = line.split('\t')
+                userToReps[userId] = int(reputation)
+                line = labelsTrain.readline()
+    
     try:
         with open(cachingFile, 'r') as dataTrain:
             return userToReps, pd.read_csv(dataTrain)
     except:
-        postsMatrix = []
-        try:
-            with open(dataTrainFile, "r") as dataTrain:
-                line = dataTrain.readline()
-                # print("now here")
-                while (line):
-                    textualDescription, userId, contentType, timeStamp = line.split('\t')
-
-                    polarity, subjectivity = textBlockToSentiment(textualDescription)
-                    readability, avg_length = textBlockToReadability(textualDescription)
-
-                    postsMatrix.append([polarity, subjectivity, readability, avg_length, contentTypes[contentType], userToReps[userId] ])
-
-                    line = dataTrain.readline()
-
-
-            df = pd.DataFrame(postsMatrix)
-            print()
-            df.to_csv(cachingFile, header=True, index=False)
-
-        except:
-            try:
-                pass
-            except:
-                pass
-
-
-
+        postsMatrix = getPostsMatrix(dataTrainFile, cachingFile)
+    
     return userToReps, postsMatrix
+    
+    
+def getTestDictionary(dataTestFile):
+    postsMatrix = getPostsMatrix(dataTestFile, None)
+    return postsMatrix
 
-
-
-
-first, second = getDictionaries()
